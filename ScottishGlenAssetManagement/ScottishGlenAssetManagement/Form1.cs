74bc5ae2688ce241dcd33b10b1443929da2f7d71
Lawrie Wilson <2300635@uad.ac.uk>
2025-11-16 19:26:29 +0000

    Created .NET application

---
+++ ScottishGlenAssetManagement/ScottishGlenAssetManagement/Form1.cs
@@ -0,0 +1,22 @@
+namespace ScottishGlenAssetManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(497, 405);
+            Name = "Form1";
+            ResumeLayout(false);
+
+        }
+    }
+}
---
+++ ScottishGlenAssetManagement/ScottishGlenAssetManagement/Program.cs
@@ -0,0 +1,20 @@
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+namespace ScottishGlenAssetManagement
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
